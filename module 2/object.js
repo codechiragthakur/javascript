6dc4b84b17ac807eff52af81a19fde072a6d1e34
chirag <MY_NAME@example.com>
2022-03-15 17:49:24 +0530

    objectcmplt

--- module 2/object.js
+++ module 2/object.js
@@ -1,7 +1,26 @@
-let objchirag = {
-    Name: "chirag",
+let obj = {
+    name: "chirag",
     eduction: "3rdyear",
-    age: 21
+    age: 21,
+    address: {
+        city: "delhi",
+        pincode: 110042,
+    },
+    friends: ['natasha', 'ajay']
+
 
 }
-console.log(objchirag);
+console.log(obj);
+
+obj.movies = ['cakhonapyaarhai', 'krish'];
+console.log(obj);
+delete obj.movies;
+console.log(obj);
+obj.address.city = 'newyork';
+console.log(obj);
+// console.log(obj.name);
+// another method to acess
+console.log('name')
+
+// git branch -M main
+// git push -u origin main
